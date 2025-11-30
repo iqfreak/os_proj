a9bec0ea2b7fcf111f0688124b3932d375aa25de
zeyad-shaban <zeyadshapan2004@gmail.com>
2025-11-30 12:30:12 +0200

    copy working

--- user/cp.c
+++ user/cp.c
@@ -1,20 +1,12 @@
-#include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/types.h"
 #include "user/user.h"
 
 /*mv src dst*/
-int main(int argc, char *argv[])
-{
-  if (argc != 3)
-  {
-    fprintf(2, "mv: Incorrect usage, mv src dst\n");
-    exit(1);
-  }
-
-  // int statusCode = move(argv[1], argv[2], 0, err_msg);
-  // if (statusCode < 0)
-  // {
-  //   fprintf(2, "%s", err_msg);
-  // }
-  // exit(statusCode);
+int main(int argc, char *argv[]) {
+    exit(move(argv[1], argv[2], 0));
+    if (argc != 3) {
+        fprintf(2, "cp: Incorrect usage, cp src dst\n");
+        exit(1);
+    }
 }
